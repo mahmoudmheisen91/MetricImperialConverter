2072036c8657f4f15745b4d49b08ab3765d90410
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-09 00:15:04 +0300

    add helemet
    US1, US2

--- server.js
+++ server.js
@@ -3,17 +3,18 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let cors = require("cors");
-
-let expect = require("chai").expect;
+let helmet = require("helmet");
 
 let apiRoutes = require("./routes/api.js");
 let fccTestingRoutes = require("./routes/fcctesting.js");
 let runner = require("./test-runner.js");
 
 let app = express();
+app.use(helmet());
 
 // Testing (For FCC testing purposes only):
 app.use(cors({ origin: "*" }));
+// process.env.NODE_ENV = "test";
 
 // This project needs to parse POST bodies:
 app.use(bodyParser.json());
@@ -21,15 +22,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CSS/HTML (Index page, static HTML/CSS):
 app.use(express.static("public"));
-// app.use("/public", express.static(process.cwd() + "/public"));
 app.route("/").get((req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API
+// Routing for API
 apiRoutes(app);
 
 // Not found middleware:
@@ -37,26 +37,20 @@ app.use((req, res, next) => {
   return next({ status: 404, message: "not found" });
 });
 
-// PORT:
+// Start our server and tests!
 const listener = app.listen(3000 || process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch (error) {
+        console.log("Tests are not valid:");
+        console.log(error);
+      }
+    }, 1500);
+  }
 });
-
-//Start our server and tests!
-// app.listen(3000 || process.env.PORT, function () {
-//   console.log("Listening on port " + process.env.PORT);
-//   if (process.env.NODE_ENV === "test") {
-//     console.log("Running Tests...");
-//     setTimeout(function () {
-//       try {
-//         runner.run();
-//       } catch (e) {
-//         var error = e;
-//         console.log("Tests are not valid:");
-//         console.log(error);
-//       }
-//     }, 1500);
-//   }
-// });
 
 module.exports = app; //for testing
