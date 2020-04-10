dccd641b38de8dac0cca62b1ba87c0331269c9df
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-04-10 12:05:56 +0300

    implement api test

--- routes/api.js
+++ routes/api.js
@@ -12,46 +12,61 @@ var expect = require("chai").expect;
 var ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  var convertHandler = new ConvertHandler();
+  app.route("/api/convert/api_test/").post((req, res) => {
+    let input = req.body.valueunit;
+    let obj = process(input);
+
+    res.json(obj);
+    res.end();
+  });
 
   app.route("/api/convert").get(function (req, res) {
     var input = req.query.input;
-    var initNum = convertHandler.getNum(input);
-    var initUnit = convertHandler.getUnit(input);
-    let result = "";
+    let obj = process(input);
 
-    if (initNum !== "invalid number")
-      var returnNum = convertHandler.convert(initNum, initUnit);
-    else result = "invalid number";
-
-    if (initUnit !== "invalid unit")
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-    else if (result === "invalid number") result = "invalid number and unit";
-    else result = "invalid unit";
-
-    if (
-      result === "invalid number" ||
-      result === "invalid unit" ||
-      result === "invalid number and unit"
-    ) {
-      res.json({ result });
-    } else {
-      var toString = convertHandler.getString(
-        initNum,
-        initUnit,
-        returnNum,
-        returnUnit
-      );
-
-      let obj = {
-        initNum,
-        initUnit,
-        returnNum,
-        returnUnit,
-        string: toString,
-      };
-
-      res.json(obj);
-    }
+    res.json(obj);
+    res.end();
   });
 };
+
+function process(input) {
+  var convertHandler = new ConvertHandler();
+
+  let initNum = convertHandler.getNum(input);
+  let initUnit = convertHandler.getUnit(input);
+  let result = "";
+
+  if (initNum !== "invalid number")
+    var returnNum = convertHandler.convert(initNum, initUnit);
+  else result = "invalid number";
+
+  if (initUnit !== "invalid unit")
+    var returnUnit = convertHandler.getReturnUnit(initUnit);
+  else if (result === "invalid number") result = "invalid number and unit";
+  else result = "invalid unit";
+
+  if (
+    result === "invalid number" ||
+    result === "invalid unit" ||
+    result === "invalid number and unit"
+  ) {
+    return { result };
+  } else {
+    var toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    let obj = {
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string: toString,
+    };
+
+    return obj;
+  }
+}
